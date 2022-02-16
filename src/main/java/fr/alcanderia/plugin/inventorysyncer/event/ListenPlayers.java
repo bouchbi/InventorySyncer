da19826785d4ec1112e4b34808695e8295e233d1
bibouche_ <mil.somnn@gmail.com>
2022-02-16 10:27:50 +0100

    added option syncOnJoinAndLeave.ec and .inv

--- src/main/java/fr/alcanderia/plugin/inventorysyncer/event/ListenPlayers.java
+++ src/main/java/fr/alcanderia/plugin/inventorysyncer/event/ListenPlayers.java
@@ -12,24 +12,39 @@ public class ListenPlayers implements Listener {
 	
 	@EventHandler
 	public static void onPlayerQuit(PlayerQuitEvent e) throws IOException {
-		if (Objects.equals(InventorySyncer.getConfiguration().getString("dataStorage"), "mysql")) {
-			InventoryWriter.writeInvToDB(e.getPlayer());
-		} else {
-			InventoryWriter.writeInvToFile(e.getPlayer());
+		if (InventorySyncer.getConfiguration().getBoolean("syncOnJoinAndLeave.inv")) {
+			if (Objects.equals(InventorySyncer.getConfiguration().getString("dataStorage"), "mysql")) {
+				InventoryWriter.writeInvToDB(e.getPlayer());
+			}
+			else {
+				InventoryWriter.writeInvToFile(e.getPlayer());
+			}
 		}
-		
+		if (InventorySyncer.getConfiguration().getBoolean("syncOnJoinAndLeave.ec")) {
+			if (Objects.equals(InventorySyncer.getConfiguration().getString("dataStorage"), "mysql")) {
+				InventoryWriter.writeECToDB(e.getPlayer());
+			}
+			else {
+				InventoryWriter.writeECToFile(e.getPlayer());
+			}
+		}
 	}
 	
 	@EventHandler
 	public static void onPlayerJoin(PlayerJoinEvent e) {
-		if (!InventorySyncer.getConfiguration().getBoolean("enablePickUpOnSync")) {
-			e.getPlayer().setCanPickupItems(false);
+		if (InventorySyncer.getConfiguration().getBoolean("syncOnJoinAndLeave.inv")) {
+			if (!InventorySyncer.getConfiguration().getBoolean("enablePickUpOnSync")) {
+				e.getPlayer().setCanPickupItems(false);
+			}
+			
+			InventoryReader.readInvAndApply(e.getPlayer(), Objects.equals(InventorySyncer.getConfiguration().getString("dataStorage"), "mysql"));
+			if (!InventorySyncer.getConfiguration().getBoolean("enablePickUpOnSync")) {
+				e.getPlayer().setCanPickupItems(true);
+			}
 		}
 		
-		InventoryReader.readInvAndApply(e.getPlayer(), Objects.equals(InventorySyncer.getConfiguration().getString("dataStorage"), "mysql"));
-		if (!InventorySyncer.getConfiguration().getBoolean("enablePickUpOnSync")) {
-			e.getPlayer().setCanPickupItems(true);
-		}
-		
+		if (InventorySyncer.getConfiguration().getBoolean("syncOnJoinAndLeave.ec"))
+			InventoryReader.readECInvAndApply(e.getPlayer(), Objects.equals(InventorySyncer.getConfiguration().getString("dataStorage"), "mysql"));
 	}
+	
 }
