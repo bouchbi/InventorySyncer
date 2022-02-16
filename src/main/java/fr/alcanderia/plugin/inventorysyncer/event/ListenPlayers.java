40b2a55c2d5bb99301440e71aa252a6f4ae71d51
bibouche_ <mil.somnn@gmail.com>
2022-02-16 11:37:29 +0100

    fixed inv not restoring on join issue

--- src/main/java/fr/alcanderia/plugin/inventorysyncer/event/ListenPlayers.java
+++ src/main/java/fr/alcanderia/plugin/inventorysyncer/event/ListenPlayers.java
@@ -32,19 +32,18 @@ public class ListenPlayers implements Listener {
 	
 	@EventHandler
 	public static void onPlayerJoin(PlayerJoinEvent e) {
-		if (InventorySyncer.getConfiguration().getBoolean("syncOnJoinAndLeave.inv")) {
-			if (!InventorySyncer.getConfiguration().getBoolean("enablePickUpOnSync")) {
-				e.getPlayer().setCanPickupItems(false);
-			}
-			
-			InventoryReader.readInvAndApply(e.getPlayer(), Objects.equals(InventorySyncer.getConfiguration().getString("dataStorage"), "mysql"));
-			if (!InventorySyncer.getConfiguration().getBoolean("enablePickUpOnSync")) {
-				e.getPlayer().setCanPickupItems(true);
-			}
-		}
-		
 		if (InventorySyncer.getConfiguration().getBoolean("syncOnJoinAndLeave.ec"))
 			InventoryReader.readECInvAndApply(e.getPlayer(), Objects.equals(InventorySyncer.getConfiguration().getString("dataStorage"), "mysql"));
+		
+		if (InventorySyncer.getConfiguration().getBoolean("syncOnJoinAndLeave.inv")) {
+			if (!InventorySyncer.getConfiguration().getBoolean("enablePickUpOnSync"))
+				e.getPlayer().setCanPickupItems(false);
+			
+			InventoryReader.readInvAndApply(e.getPlayer(), Objects.equals(InventorySyncer.getConfiguration().getString("dataStorage"), "mysql"));
+			if (!InventorySyncer.getConfiguration().getBoolean("enablePickUpOnSync"))
+				e.getPlayer().setCanPickupItems(true);
+		}
+		
 	}
 	
 }
